60f75e04155ada0c84e3fead23a154fa39665fa1
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-07 12:38:09 +0100

    Add embedded video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import { useState } from "react";
+import ReactPlayer from "react-player";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [showVideo, setShowVideo] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {showVideo ? <Video /> : <Logo />}
+
+        <button
+          className={showVideo ? "button-alt-color" : ""}
+          onClick={() => setShowVideo(!showVideo)}
+        >
+          {showVideo ? "CLOSE" : "OPEN"}
+        </button>
       </header>
+    </div>
+  );
+}
+
+function Logo() {
+  return <img src={logo} className="App-logo" alt="logo" />;
+}
+
+function Video() {
+  const url = "https://www.youtube.com/watch?v=VR9LEqq6kYc";
+  return (
+    <div className="player-wrapper">
+      <ReactPlayer url={url} width="100%" height="100%" />
     </div>
   );
 }
